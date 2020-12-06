741b2573e853283bacbc8d3064255f5237ac356c
evanhayes <evanhayes66@yahoo.com>
2020-12-06 17:04:25 -0500

    ADDED java version

---
+++ src/main/resources/static/precache-manifest.e7b76f97f86b43da74d202c857618f7c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea31fc7b8b292e554df094e9e3a4d83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a0fecb44af84e488448",
+    "url": "/static/css/2.9dbb8234.chunk.css"
+  },
+  {
+    "revision": "d44b129faed506457495",
+    "url": "/static/css/main.12eb8dff.chunk.css"
+  },
+  {
+    "revision": "7a0fecb44af84e488448",
+    "url": "/static/js/2.9d28041e.chunk.js"
+  },
+  {
+    "revision": "f12839b2eb96790ff6590debcc216caa",
+    "url": "/static/js/2.9d28041e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d44b129faed506457495",
+    "url": "/static/js/main.49990b20.chunk.js"
+  },
+  {
+    "revision": "d0c9c618e296f2dbcc57",
+    "url": "/static/js/runtime-main.e8868bbc.js"
+  },
+  {
+    "revision": "487bcc79922e11600d15069042f27100",
+    "url": "/static/media/coming-soon-image.487bcc79.jpg"
+  },
+  {
+    "revision": "6ab600345821967e24a9e70e14d8dea2",
+    "url": "/static/media/darkMovieBG.6ab60034.jpg"
+  },
+  {
+    "revision": "f1d7b76d49ba5363944c01e9b1d4915c",
+    "url": "/static/media/logo.f1d7b76d.png"
+  }
+]);
